0d2da893e6dca110214eee82deff456a4ae972a7
Nicolas <nicolaspierina@gmai.com>
2025-12-07 17:40:36 -0300

    css Modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -16,8 +16,8 @@ export const Modal = ({ isModalOpen, onClose, onConfirm, type }) => {
   } = infosModal;
 
   const loader = (
-    <div className="flex justify-center items-center h-40">
-      <div className="w-12 h-12 border-4 border-blue-400 border-dashed rounded-full animate-spin"></div>
+    <div className="loader-container">
+      <div className="loader-spinner"></div>
     </div>
   );
 
