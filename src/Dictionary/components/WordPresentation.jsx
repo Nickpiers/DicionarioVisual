ad09481f23e489b9181ff01d6ca66123b15603f6
Nicolas <nicolaspierina@gmai.com>
2025-12-07 15:48:45 -0300

    adicionando react player

--- src/Dictionary/components/WordPresentation.jsx
+++ src/Dictionary/components/WordPresentation.jsx
@@ -1,5 +1,6 @@
 import { MeaningCards } from "./MeaningCards";
 import { WelcomeScreen } from "./WelcomeScreen";
+import ReactPlayer from "react-player";
 
 export const WordPresentation = ({ selectedWord }) => {
   if (!selectedWord) {
@@ -16,11 +17,9 @@ export const WordPresentation = ({ selectedWord }) => {
       <h2 className="text-4xl font-bold">{title}</h2>
       <hr className="my-4 border-t border-gray-300" />
       {video && (
-        <video
-          src={`/assets/${video}`}
-          controls
-          className="w-full max-w-3xl mx-auto mb-6 rounded-lg shadow-md"
-        />
+        <div className="w-full max-w-3xl mx-auto mb-6 rounded-lg shadow-md">
+          <ReactPlayer src={video} controls width="100%" height="480px" />
+        </div>
       )}
       <div className="flex flex-wrap gap-6 w-full mt-4">
         {meanings.map(({ icon, definition, example }, index) => (
